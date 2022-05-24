924f6d03b540f3ea06d9060b44e0bf395872d84a
Arjunanda <vincentiusmandala@gmail.com>
2022-05-25 03:23:00 +0700

    test

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -188,7 +188,7 @@ class ProductService
 
     public function getCheckout(Request $request)
     {
-        $product = $request->user()->member;
+        $product = $request->user()->member->bidder;
         return $product;
     }
 }
