9fea1f244cbcc70fb6ef5cff602d37f528e0919a
Arjunanda <vincentiusmandala@gmail.com>
2022-05-24 16:21:19 +0700

    heroku testing

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -125,19 +125,18 @@ class ProductService
             throw new HttpResponseException(response()->json($response, 422));
         }
         // Check if bidder is product owner
-        if (!empty($request->user()->member->store)) {
-            $is_owner = Product::query()
-                ->where('store_id', $request->user()->member->store->id)
-                ->where('id', $request->product_id)
-                ->count() > 0
-                ? true
-                : false;
-            if ($is_owner) {
-                $response['status'] = false;
-                $response['message'] = 'Product owner can\'t bid on your own product!';
+        dd($request->user()->member);
+        $is_owner = Product::query()
+            ->where('store_id', $request->user()->member->store->id)
+            ->where('id', $request->product_id)
+            ->count() > 0
+            ? true
+            : false;
+        if ($is_owner) {
+            $response['status'] = false;
+            $response['message'] = 'Product owner can\'t bid on your own product!';
 
-                throw new HttpResponseException(response()->json($response, 422));
-            }
+            throw new HttpResponseException(response()->json($response, 422));
         }
 
         $model = new ProductBidder();
