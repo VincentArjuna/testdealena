81a775bd2a5e932059e634f4e92395f480d978df
Arjunanda <vincentiusmandala@gmail.com>
2022-05-29 04:41:59 +0700

    asd

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -7,7 +7,8 @@ echo "Deployment started ..."
 (php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
 
 # Update codebase
-git pull
+git fetch
+git reset --hard origin/main
 
 # Install composer dependencies
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader --no-progress
